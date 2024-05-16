f48b39685a4443b279179469ce7c54c357ee34da
Clediano Estefenon <cledianoestefenon@MacBook-Pro-de-Gislaine.local>
2024-05-15 21:08:45 -0300

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('home').then(c => c.HomeComponent),
+    },
+    {
+        path: 'users',
+        loadComponent: () => import('users').then(c => c.UsersComponent),
+    }
+];
